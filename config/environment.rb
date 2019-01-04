96ae42244636aba48a6561794af80176cdf8dc8b
adudley78 <dudleyadam@gmail.com>
2019-01-04 15:09:52 -0500

    Cleaned up comments in all files and deleted unnecessary scraper.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@
 # require 'nokogiri'
 # require 'open-uri'
 #
-# require_relative '../lib/worlds_best_beaches/scraper'
 # require_relative '../lib/worlds_best_beaches/beach'
 # require_relative '../lib/worlds_best_beaches/cli'
 # require_relative '../lib/worlds_best_beaches/version'
