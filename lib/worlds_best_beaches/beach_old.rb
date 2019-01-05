23cbd90fe7bbdda2f2f9f4ffa7a852006e826678
adudley78 <dudleyadam@gmail.com>
2019-01-05 11:15:34 -0500

    Reworking Nokogiri code b/c scraping new site.

--- lib/worlds_best_beaches/beach_old.rb
+++ lib/worlds_best_beaches/beach_old.rb
@@ -1,29 +1,30 @@
+# instatiates a new parent class and child class
 class WorldsBestBeaches::Beach
 
   # creates callable methods or attributes or properties for each Beach instance
-  attr_accessor :name, :city, :country, :insta_appeal, :fun_fact, :travel_tips # might want to add best time of year to visit in the future
+  attr_accessor :name, :location, :more_info # might want to add best time of year to visit in the future
 
   # returns a collection of instances of Beach objects
   def self.all
     self.scrape_beaches
   end
 
+  # going to need an initialize method and I want to use .send to practice mass assignment if possible
+
   def self.scrape_beaches
     beaches = []
-    beaches << self.scrape_forbes
+    beaches << self.scrape_tripadvisor
     beaches
   end
 
   # scrapes the best beaches webpage and assigns predefined attributes to Beach objects
-  def self.scrape_forbes
-    doc = Nokogiri::HTML(open("https://www.forbes.com/sites/laurabegleybloom/2017/11/27/ranked-the-worlds-50-best-beaches"))
+  def self.scrape_tripadvisor
+    doc = Nokogiri::HTML(open("https://www.coastalliving.com/tripadvisor-best-beaches-world"))
+    binding.pry
     beach = self.new
-    beach.name = doc.css("p strong")[0].text.split(",")[0].sub!( /\A.{3}/m, "" ) # << gives me the name of the first beach, increment by 4
-    beach.city = doc.css("p strong")[4].text.split(",")[1].lstrip # << city of first beach
-    beach.country = doc.css("p strong")[0].text.split(",")[2].lstrip # << country of first beach
-    beach.insta_appeal = doc.css("p")[7].text
-    beach.fun_fact = doc.css("p")[8].text
-    beach.travel_tips = doc.css("p")[9].text
+    beach.name = doc.css('.glide-slide.image-slide')[24].css('h3').text.strip.sub!( /\A.{3}/m, "" ) # << gives me the name of the first beach
+    beach.location = location = doc.css('.glide-slide.image-slide')[24].css('div.glide-slide-desc p a strong').text.strip # << location of first beach
+    beach.more_info = 
     beach
   end
 end
