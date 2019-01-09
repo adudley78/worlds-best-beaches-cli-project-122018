912fd44f96138d4841697a254933dfc0b8296e0a
adudley78 <dudleyadam@gmail.com>
2019-01-09 16:33:06 -0500

    Clean up CLI output

--- lib/worlds_best_beaches/cli.rb
+++ lib/worlds_best_beaches/cli.rb
@@ -6,12 +6,14 @@ class WorldsBestBeaches::CLI
     WorldsBestBeaches::Beach.today
     list_beaches
     menu
-    goodbye
   end
 
   # lists beaches, data scraped from site
   def list_beaches
-    puts "Welcome to the World's Best Beaches - Discover, Learn, Explore"
+    puts " "
+    puts "Welcome to the World's Best Beaches!"
+    puts "Where do YOU want to explore today?"
+    puts " "
     # Beach is a class method that calls all that returns the collection of beaches.
     #binding.pry
     @beaches = WorldsBestBeaches::Beach.all
@@ -24,24 +26,28 @@ class WorldsBestBeaches::CLI
   def menu
     input = nil
     while input != "exit"
+      puts " "
       puts "Please enter the number of the beach you'd like to learn more about or type list to see the list of beaches again or type exit."
       input = gets.strip.downcase
       if input.to_i > 0 && input.to_i < 26
         beach = @beaches[input.to_i-1]
-        # @beaches.each do |beach,i|
+          puts " "
           puts "#{beach.more_info}"
       elsif input == "list"
         list_beaches
       elsif input == "exit"
         goodbye
       else
+        puts " "
         puts "We're not sure what you want. Type a number, list, or exit." # << This is displayed before the puts of the goodbye method when user enters 'exit'. Need to fis.
       end
     end
   end
 
     def goodbye
+      puts " "
       puts "Thanks for using Worlds Best Beaches. Come back soon!"
+      puts " "
     end
 
   end
